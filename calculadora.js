77243ad9ac5099dee8625e070ef0f911dc9ecbb4
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-10-31 10:39:38 -0300

    added basic operations like rest, multiplication and division

--- calculadora.js
+++ calculadora.js
@@ -1,7 +1,7 @@
 
 
 const state = {
-    result: [0],
+    result: "0",
 }
 
 function template(){
@@ -21,9 +21,57 @@ function render(){
 
 function igual(){
 
-    let valor = state.result[0] + state.result[2];
+    let resultado = 0;
 
-    return valor;
+
+    for(let i = 0; i<= state.result.length;i++){
+
+        if(state.result[i] === "*"){
+
+            let signo = state.result.split("*");
+            let valor1 = Number.parseInt(signo[0]);
+            let valor2 = Number.parseInt(signo[1]);
+
+            resultado = valor1*valor2;
+
+        }else if(state.result[i] === "/"){
+
+            let signo = state.result.split("/");
+            let valor1 = Number.parseInt(signo[0]);
+            let valor2 = Number.parseInt(signo[1]);
+            
+            if(valor2 === 0){
+                alert("No es posible dividir por 0")
+            }else{
+                resultado = valor1/valor2;
+            };
+            
+
+        }else if(state.result[i] === "+"){
+            let valor = state.result.split("+");
+            // comvertimos los string a numeros
+            let valor1 = Number.parseInt(valor[0]);
+            let valor2 = Number.parseInt(valor[1]);
+        
+            resultado = valor1+valor2;
+
+        }else if(state.result[i] === "-"){
+            let valor = state.result.split("-");
+            // comvertimos los string a numeros
+            let valor1 = Number.parseInt(valor[0]);
+            let valor2 = Number.parseInt(valor[1]);
+        
+            resultado = valor1-valor2;
+
+        }
+
+
+
+    }
+
+
+
+    return resultado;
 
 }
 
@@ -40,12 +88,13 @@ function calculadora(){
             console.log("estado previo del state",state);
             console.log("Agregamos el valor 1 al state");
 
-            if(state.result[0] === 0){
-                state.result[0] = 1;
+
+            if(state.result === "0"){
+                state.result = "1";
             }else{
-                state.result.push(1);
+                state.result = state.result += "1";
             }
-            
+           
    
         }
 
@@ -54,23 +103,62 @@ function calculadora(){
             console.log("Agregamos el valor 2 al state");
 
 
-            if(state.result[0] === 0){
-                state.result[0] = 2;
+            if(state.result === "0"){
+                state.result = "2";
             }else{
-                state.result.push(2);
+                state.result = state.result += "2";
             }
             
-     
+            
         }
+
+        if(e.target.matches(".btn0")){
+            console.log("estado previo del state",state);
+            console.log("Agregamos el valor 2 al state");
+
+
+            if(state.result === "0"){
+                state.result = "0";
+            }else{
+                state.result = state.result += "0";
+            }
+            
+            
+        }
+
+        if(e.target.matches(".multiplicar")){
+            console.log("estado previo del state",state);
+            console.log("Agregamos el valor 2 al state");
+
+            state.result = state.result += "*";
+        }
+
+
+        if(e.target.matches(".dividir")){
+            console.log("estado previo del state",state);
+            console.log("Agregamos el valor 2 al state");
+
+            state.result = state.result += "/";
+        }
+
 
         if(e.target.matches(".mas")){
             console.log("estado previo del state",state);
             console.log("Agregamos el valor 2 al state");
-            state.result.push("+");
+
+            state.result = state.result += "+";
+        }
+
+        if(e.target.matches(".menos")){
+            console.log("estado previo del state",state);
+            console.log("Agregamos el valor 2 al state");
+
+            state.result = state.result += "-";
         }
 
         if(e.target.matches(".igual")){
-            state.result.push(igual());
+            
+            console.log(igual());
             
         }
 
