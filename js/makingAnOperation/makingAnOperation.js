c3072e6241901507e0f3c9ef5c618474980126ad
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-11-10 10:41:52 -0300

    now the function makingAnOperation has his own file

---
+++ js/makingAnOperation/makingAnOperation.js
@@ -0,0 +1,49 @@
+
+const makingAnOperation = (stateCopy,signoString) =>{
+    // determinamos que signo es y dividimos el string
+    let result = 0,
+    valor = stateCopy.operation.split(signoString);
+    console.log(valor[0]);
+    console.log(stateCopy.result);
+
+    // comvertimos los string a numeros
+    let valor1 = Number.parseFloat(valor[0]);
+    let valorR = Number.parseFloat(stateCopy.result);
+
+    if(stateCopy.operation.endsWith("*")){
+        console.log("Multiplicar");
+        result = valor1 * valorR;
+        console.log(`valor 1: ${valor1} * Valor: ${valorR} = Resultado`,result);
+        stateCopy.operation = `${result}${signoString}`;
+        stateCopy.result = `${result}`;
+    }
+
+    if(stateCopy.operation.endsWith("/")){
+        console.log("Dividir");
+        if(valorR === 0) {alert("No es posible dividir por 0"); return;} 
+        result = valor1 / valorR;
+        console.log(`valor 1: ${valor1} / Valor: ${valorR} = Resultado`,result);
+        stateCopy.operation = `${result}${signoString}`;
+        stateCopy.result = `${result}`;
+    }
+
+    if(stateCopy.operation.endsWith("+")){
+        console.log("suman");
+        result = valor1 + valorR;
+        console.log(`valor 1: ${valor1} + Valor: ${valorR} = Resultado`,result);
+        stateCopy.operation = `${result}${signoString}`;
+        stateCopy.result = `${result}`;
+    }
+    
+
+    if(stateCopy.operation.endsWith("-")){
+        console.log("Resta");
+        result = valor1 - valorR;
+        console.log(`valor 1: ${valor1} - Valor: ${valorR} = Resultado`,result);
+        stateCopy.operation = `${result}${signoString}`;
+        stateCopy.result = `${result}`;
+    }
+
+}
+
+export default makingAnOperation;
