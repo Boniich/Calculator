269502bd577b581780aef9a44b851025be50e3aa
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-11-17 16:16:19 -0300

    this file before was 'operationsAndResults', just has a change of name, even his folder

---
+++ js/helpers/operations/operations.js
@@ -0,0 +1,41 @@
+const operations = (stateCopy,signo,value1,value2) =>{
+
+    let result = 0;
+
+    switch (signo) {
+
+        case "+": 
+            console.log("Esto es una suma");
+            result = value1 + value2;
+            console.log("Este es el valor desde operationsAndResults",result);
+            break;
+
+        case "-": 
+            console.log("Esto es una resta");
+            result = value1 - value2;
+            console.log("Este es el valor desde operationsAndResults",result);
+            break;
+
+        case "*": 
+            console.log("Esto es una multiplicacion");
+            result = value1 * value2;
+            console.log("Este es el valor desde operationsAndResults",result);
+            break;
+
+        case "÷": 
+            console.log("Esto es una division");
+            result = value1 / value2;
+            console.log("Este es el valor desde operationsAndResults",result);
+            break;    
+    
+        default:
+            break;
+    }
+
+
+    stateCopy.operation = `${result}${signo}`;
+    stateCopy.result = `${result}`;
+
+}
+
+export default operations;
