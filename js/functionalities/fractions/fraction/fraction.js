68fb89ca158b0285c887f6ae042cd7cbe568e4e3
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-11-17 19:02:23 -0300

    function for when will do click in the button 1(x) is now in a separated file

---
+++ js/functionalities/fractions/fraction/fraction.js
@@ -0,0 +1,23 @@
+import mathExpressions from "../../mathExpressions/mathExpressions.js";
+
+const fraction = (stateCopy,flag) =>{
+
+    let operation = new String(stateCopy.operation);
+    let result = stateCopy.result;  
+    // Esta expression regular sirve para casos como "1/(10)+"
+    let expression = /(1\/\(\-?[0-9 | .]+\))./g;
+    // Esta expression regular sirve para casos como "3+"
+    let expression2 = /([0-9 | .]+[\+ | \- | \* | \÷])/g
+
+    let exp1 = expression.test(operation);
+    let exp2 = expression2.test(operation);
+
+    if(result === "0"){
+        alert("No es posible dividir por 0");
+        return;
+    }
+
+    mathExpressions(stateCopy,flag,exp1,exp2,`1/(${result})`,1/result);
+}
+
+export default fraction;
