b4836e8a2e6fdf5d312aae6c41fb983532be825a
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-11-18 19:36:21 -0300

    types of operations with squareRoot

---
+++ js/functionalities/allSquareRoot/numberAndSquareRoot/numberAndSquareRoot.js
@@ -0,0 +1,26 @@
+import testingOperation from "../../../helpers/testingOperations/testingOperations.js";
+
+const numberAndSquareRoot = (stateCopy,operation) =>{
+
+    console.log("Number and squareRoot");
+
+    let extractNumbers = /([0-9 | .]+).√\(([0-9 | .]+)\)/g;
+    let numbers = extractNumbers.exec(operation);
+    console.log(numbers);
+
+    let value1 = Number.parseFloat(numbers[1]);
+    let value2 = Number.parseFloat(numbers[2]);
+
+    value2 = Math.sqrt(value2);
+
+    // operations
+    let sum = /([0-9 | .]+)\+(√\([0-9 | .]+\))/g;
+    let subtract = /([0-9 | .]+)\-(√\([0-9 | .]+\))/g;
+    let multiply = /([0-9 | .]+)\*(√\([0-9 | .]+\))/g;
+    let division = /([0-9 | .]+)\÷(√\([0-9 | .]+\))/g;
+
+    testingOperation(stateCopy,operation,sum,subtract,multiply,division,value1,value2);
+
+}
+
+export default numberAndSquareRoot;
---
+++ js/functionalities/allSquareRoot/squareRootAndNumber/squareRootAndNumber.js
@@ -0,0 +1,27 @@
+import testingOperation from "../../../helpers/testingOperations/testingOperations.js";
+
+const squareRootAndNumber = (flag,stateCopy,operation) =>{
+
+    console.log("SquareRoot and Number");
+
+    let extractNumbers = /√\(([0-9 | .]+)\)/g;
+    let numbers = extractNumbers.exec(operation);
+    console.log(numbers);
+
+    let value1 = Number.parseFloat(stateCopy.result);
+    let value2 = Number.parseFloat(numbers[1]);
+
+    value1 = Math.sqrt(value1);
+
+    // operations
+    let sum = /(√\([0-9 | .]+\)\+$)/g
+    let subtract = /(√\([0-9 | .]+\)\-$)/g
+    let multiply = /(√\([0-9 | .]+\)\*$)/g
+    let division = /(√\([0-9 | .]+\)\÷$)/g
+
+    testingOperation(stateCopy,operation,sum,subtract,multiply,division,value1,value2);
+    flag.overWrite = true;
+
+}
+
+export default squareRootAndNumber;
