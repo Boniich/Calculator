a531c3ea47a656af9672699d2c28c7ad36922f81
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-11-21 18:04:56 -0300

    now is possible add a fraction or potency with a squareRoot for make an combinated operation

--- js/functionalities/allSquareRoot/squareRoot/squareRoot.js
+++ js/functionalities/allSquareRoot/squareRoot/squareRoot.js
@@ -5,12 +5,20 @@ const squareRoot = (stateCopy,flag) =>{
 
     let operation = new String(stateCopy.operation);
     let result = stateCopy.result;  
-
+    let validateCom = false;
     let expression = /(√\([0-9 | .]+\).)/g;
     let expression2 = /([0-9 | .]+[\+ | \- | \x | \÷])/g
+    let fraction_squareRoot = /(1\/\(\-?[0-9 | .]+\))./g;
+    let potency_squareRoot = /(sqr\(\-?[0-9 | .]+\).)/g;
 
     let exp1 = expression.test(operation);
     let exp2 = expression2.test(operation);
+    let testFraction_squareRoot = fraction_squareRoot.test(operation);
+    let testPotency_squareRoot = potency_squareRoot.test(operation);
+
+    if(testFraction_squareRoot === true || testPotency_squareRoot === true){
+        validateCom = true;
+    }
 
     let negativeNumbers = /(\-[0-9 | .])/g;
     let validate = negativeNumbers.test(result);
@@ -24,7 +32,7 @@ const squareRoot = (stateCopy,flag) =>{
     let potencyExpression = `√(${result})`;
     let potencySolved = Math.sqrt(result);
 
-    mathExpressions(stateCopy,flag,exp1,exp2,potencyExpression,potencySolved);
+    mathExpressions(stateCopy,flag,exp1,exp2,validateCom,potencyExpression,potencySolved);
 
 }
 
