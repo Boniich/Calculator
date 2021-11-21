5d9557692d04877431ad65149bad8ee49bb43d78
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-11-21 18:07:36 -0300

    mathexpression recive one prop more for handle combinated operations

--- js/functionalities/mathExpressions/mathExpressions.js
+++ js/functionalities/mathExpressions/mathExpressions.js
@@ -1,6 +1,6 @@
-const mathExpressions = (stateCopy,flag,exp1,exp2,renderMathExpression,solveMathExpression) =>{
+const mathExpressions = (stateCopy,flag,exp1,exp2,testFraction_potency,renderMathExpression,solveMathExpression) =>{
 
-    if(exp1 === true || exp2 === true){
+    if(exp1 === true || exp2 === true || testFraction_potency === true){
         stateCopy.operation += renderMathExpression;
         stateCopy.result = solveMathExpression;
         flag.overWrite = true;
