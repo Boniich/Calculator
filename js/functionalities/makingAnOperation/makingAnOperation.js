bf87eddf3bb997f75371b52ecc00b77718c85c11
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-11-21 18:08:39 -0300

    now combinated operations with fractions,potency and squareRoot are handle in this file

--- js/functionalities/makingAnOperation/makingAnOperation.js
+++ js/functionalities/makingAnOperation/makingAnOperation.js
@@ -1,8 +1,9 @@
-import deletePoint from "../point/deletePoint/deletePoint.js";
 import fractionsOperations from "../fractions/fractionsOperations/fractionsOperations.js";
 import potencyOperations from "../allPotency/potencyOperations/potencyOperations.js";
 import squareRootOperations from "../allSquareRoot/squareRootOperations/squareRootOperations.js";
 import basicOperations from "../basiOperations/basicOperations.js";
+import F_P_Q_Combinated from "../../fraction_potency_squareRoot_combinated/F_P_Q_Combinated/F_P_S_combinated.js";
+
 
 const makingAnOperation = (flag,stateCopy,equal) =>{
 
@@ -14,12 +15,12 @@ const makingAnOperation = (flag,stateCopy,equal) =>{
     let expres = /(1\/\(\-?[0-9 | .]+\)).(1\/\(\-?[0-9 | .]+\))/g;
     let expression2 = /(\-?[0-9 | .]+.1\/\(\-?[0-9 | .]+\))/g
     let expression3 = /(1\/\(\-?[0-9 | .]+\).$)/g
+    
 
     
     let exp1 = expres.test(operation);
     let exp2 = expression2.test(operation);
     let exp3 = expression3.test(operation);
-
 
     //potency
 
@@ -41,6 +42,24 @@ const makingAnOperation = (flag,stateCopy,equal) =>{
     let exp9 = expression9.test(operation);
 
 
+    //fractions, potency and squareRoot combinated
+    let fraction_potency = /(1\/\(\-?[0-9 | .]+\)).(sqr\(\-?[0-9 | .]+\))/g;
+    let potency_fraction = /(sqr\(\-?[0-9 | .]+\)).(1\/\(\-?[0-9 | .]+\))/g;
+    let fraction_squareRoot = /(1\/\(\-?[0-9 | .]+\)).(√\([0-9 | .]+\))/g;
+    let squareRoot_fraction = /(√\([0-9 | .]+\)).(1\/\(\-?[0-9 | .]+\))/g;
+
+    let squareRoot_potency = /(√\([0-9 | .]+\)).(sqr\(\-?[0-9 | .]+\))/g;
+    let potency_squareRoot = /(sqr\(\-?[0-9 | .]+\)).(√\([0-9 | .]+\))/g;
+
+    let testFraction_potency = fraction_potency.test(operation);
+    let testPotency_fraction = potency_fraction.test(operation);
+    let testFraction_squareRoot = fraction_squareRoot.test(operation);
+    let testSquareRoot_fraction = squareRoot_fraction.test(operation);
+    let testSquareRoot_potency = squareRoot_potency.test(operation);
+    let testPotency_squareRoot = potency_squareRoot.test(operation);
+
+
+
 
     if(exp1 === true ||exp2 === true || exp3 === true){
 
@@ -54,6 +73,17 @@ const makingAnOperation = (flag,stateCopy,equal) =>{
     }else if(exp7 === true || exp8 === true || exp9 === true){
 
         squareRootOperations(flag,stateCopy,operation,exp7,exp8,exp9,equal);
+
+    }else if(testFraction_potency === true || testPotency_fraction === true 
+        || testFraction_squareRoot === true || testSquareRoot_fraction === true
+        || testSquareRoot_potency === true || testPotency_squareRoot === true){
+        console.log("If de operaciones combinadas");
+
+        F_P_Q_Combinated(flag,stateCopy,testFraction_potency,testPotency_fraction,
+            testFraction_squareRoot,testSquareRoot_fraction,
+            testSquareRoot_potency,testPotency_squareRoot,equal);
+        
+
 
     }else{
         basicOperations(stateCopy,equal);
