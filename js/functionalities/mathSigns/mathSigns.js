e07c4b921c3c3a3fffe32396afb28ffcf56655a2
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-11-22 10:16:22 -0300

    now all regular expression are handle by a another function, in mathSigns just is called this function with a specific prop.change sign still doesnt work

--- js/functionalities/mathSigns/mathSigns.js
+++ js/functionalities/mathSigns/mathSigns.js
@@ -1,3 +1,4 @@
+import testExpression from "../../helpers/testExpression/testExpression.js";
 import makingAnOperation from "../makingAnOperation/makingAnOperation.js";
 import deletePoint from "../point/deletePoint/deletePoint.js";
 
@@ -13,49 +14,9 @@ const mathSigns = (flag,stateCopy,sign) =>{
     // let determineSign = /([\+ | \x |\÷ |\% |\= |\-])$/g
     // let thisSignIs = determineSign.exec(operation);
     // console.log("array",thisSignIs);
+    //no borrar
 
 
-    //fractions
-
-    let expres = /(1\/\(\-?[0-9 | .]+\)).(1\/\(\-?[0-9 | .]+\))/g;
-    let expression2 = /(\-?[0-9 | .]+.1\/\(\-?[0-9 | .]+\))/g
-    let exp1 = expres.test(operation);
-    let exp2 = expression2.test(operation);
-   
-
-    //potency
-
-    let expression4 = /(sqr\(\-?[0-9 | .]+\)).(sqr\(\-?[0-9 | .]+\))/g;
-    let expression5 = /(\-?[0-9 | .]+).(sqr\(\-?[0-9 | .]+\))/g
-    let exp4 = expression4.test(operation);
-    let exp5 = expression5.test(operation);
-
-    //squareRoot
-
-    let expression7 = /(√\([0-9 | .]+\)).(√\([0-9 | .]+\))/g;
-    let expression8 = /(\-?[0-9 | .]+).(√\([0-9 | .]+\))/g
-    let exp7 = expression7.test(operation);
-    let exp8 = expression8.test(operation);
-
-    // Combinate of fraction, potency and squareRoot
-
-    let fraction_potency = /(1\/\(\-?[0-9 | .]+\)).(sqr\(\-?[0-9 | .]+\))/g;
-    let potency_fraction = /(sqr\(\-?[0-9 | .]+\)).(1\/\(\-?[0-9 | .]+\))/g;
-
-    let fraction_squareRoot = /(1\/\(\-?[0-9 | .]+\)).(√\([0-9 | .]+\))/g;
-    let squareRoot_fraction = /(√\([0-9 | .]+\)).(1\/\(\-?[0-9 | .]+\))/g;
-
-    let squareRoot_potency = /(√\([0-9 | .]+\)).(sqr\(\-?[0-9 | .]+\))/g;
-    let potency_squareRoot = /(sqr\(\-?[0-9 | .]+\)).(√\([0-9 | .]+\))/g;
-
-    let testFraction_potency = fraction_potency.test(operation);
-    let testPotency_fraction = potency_fraction.test(operation);
-    let testFraction_squareRoot = fraction_squareRoot.test(operation);
-    let testSquareRoot_fraction = squareRoot_fraction.test(operation);
-    let testSquareRoot_potency = squareRoot_potency.test(operation);
-    let testPotency_squareRoot = potency_squareRoot.test(operation);
-
-    
     if(point === true || pointAndZero === true){
         console.log("boorando coma")
         deletePoint(sign,stateCopy);
@@ -69,18 +30,16 @@ const mathSigns = (flag,stateCopy,sign) =>{
 
         stateCopy.operation = "";
         stateCopy.result = "0";
-    }else if(endParentesis === true && exp1 === false && exp2 === false && exp4 === false
-        && exp5 === false && exp7 === false && exp8 === false && testFraction_potency === false
-        && testPotency_fraction === false && testFraction_squareRoot === false && 
-        testSquareRoot_fraction === false && testSquareRoot_potency === false
-        && testPotency_squareRoot === false){
+    }else if(endParentesis === true && testExpression(stateCopy,"2f") === false && testExpression(stateCopy,"nf") === false
+             && testExpression(stateCopy,"pp") === false && testExpression(stateCopy,"np") === false && testExpression(stateCopy,"ss") === false 
+             && testExpression(stateCopy,"ns") === false && testExpression(stateCopy,"fp") === false && testExpression(stateCopy,"pf") === false && 
+             testExpression(stateCopy,"fs") === false && testExpression(stateCopy,"sf") === false && testExpression(stateCopy,"sp") === false && testExpression(stateCopy,"ps") === false){
 
-        console.log("Agregar signo si termina en parentesis")
+        console.log("add sign if end with ')'")
         stateCopy.operation += sign;
 
     }else{
 
-        console.log("test desde sum")
         makingAnOperation(flag,stateCopy,equal);
     }
 
