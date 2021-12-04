85b86f6ec354744c6a767a8548dfae34e33c32ac
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-12-03 21:16:48 -0300

    this function will handle all exec function with their expressions

---
+++ js/helpers/execExpression/execExpression.js
@@ -0,0 +1,25 @@
+const execExpression = (stateCopy,typeOperation) =>{
+
+    let operation = new String(stateCopy.operation);
+    let expression;
+    let exec = [];
+
+    switch (typeOperation) {
+        case "endsWithSign":
+            expression = /(1?[\/ | \√ | sqr]*\(?\-?[0-9 | 0.0]+\)?)([\+ | \x |\÷ |\% |\= |\-])/g;
+            break;
+        case "signBetween":
+            expression = /(1?[\/ | \√ | sqr]*\(?\-?[0-9 | 0.0]+\)?)([\+ | \x |\÷ |\-])(1?[\/ | \√ | sqr]*\(?\-?[0-9 | 0.0]+\)?)/g;;
+            break;
+    
+        default:
+            break;
+    }
+
+    exec = expression.exec(operation);
+    console.log("Exe:",exec);
+    return exec;
+
+}
+
+export default execExpression;
