51e484d4aaf5c7e0fbd4c85f2e024c5319485df6
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-12-05 11:04:03 -0300

    expressions were tweaks for be more exact

--- js/helpers/testExpression/testExpression.js
+++ js/helpers/testExpression/testExpression.js
@@ -6,52 +6,52 @@ const testExpression = (stateCopy,typeOperation) =>{
     switch (typeOperation) {
         //fractions
         case "2f":
-            expression = /(1\/\(\-?[0-9 | .]+\)).(1\/\(\-?[0-9 | .]+\))/g;
+            expression = /(1\/\(\-?[0-9 | .]+\))([\+ | \x |\÷ |\-])(1\/\(\-?[0-9 | .]+\))/g;
             break;
         case "nf":
-            expression = /(\-?[0-9 | .]+.1\/\(\-?[0-9 | .]+\))/g;
+            expression = /(\-?[0-9 | .]+([\+ | \x |\÷ |\-])1\/\(\-?[0-9 | .]+\))/g;
             break;
         case "fn":
-            expression = /(1\/\(\-?[0-9 | .]+\).$)/g;
+            expression = /(1\/\(\-?[0-9 | .]+\)([\+ | \x |\÷ |\-])$)/g;
             break;
         //potency 
         case "pp":
-            expression = /(sqr\(\-?[0-9 | .]+\)).(sqr\(\-?[0-9 | .]+\))/g;
+            expression = /(sqr\(\-?[0-9 | .]+\))([\+ | \x |\÷ |\-])(sqr\(\-?[0-9 | .]+\))/g;
             break;
         case "np":
-            expression = /(\-?[0-9 | .]+).(sqr\(\-?[0-9 | .]+\))/g
+            expression = /(\-?[0-9 | .]+)([\+ | \x |\÷ |\-])(sqr\(\-?[0-9 | .]+\))/g
             break;
         case "pn":
-            expression = /(sqr\(\-?[0-9 | .]+\).$)/g;
+            expression = /(sqr\(\-?[0-9 | .]+\)([\+ | \x |\÷ |\-])$)/g;
             break;
         //square root
         case "ss":
-            expression = /(√\([0-9 | .]+\)).(√\([0-9 | .]+\))/g;
+            expression = /(√\([0-9 | .]+\))([\+ | \x |\÷ |\-])(√\([0-9 | .]+\))/g;
             break;
         case "ns":
-            expression = /(\-?[0-9 | .]+).(√\([0-9 | .]+\))/g
+            expression = /(\-?[0-9 | .]+)([\+ | \x |\÷ |\-])(√\([0-9 | .]+\))/g
             break;
         case "sn":
-            expression = /(√\([0-9 | .]+\).$)/g
+            expression = /(√\([0-9 | .]+\)([\+ | \x |\÷ |\-])$)/g
             break;
         // combinated operations with fractions, potency and squareroot
         case "fp":
-            expression = /(1\/\(\-?[0-9 | .]+\)).(sqr\(\-?[0-9 | .]+\))/g;
+            expression = /(1\/\(\-?[0-9 | .]+\))([\+ | \x |\÷ |\-])(sqr\(\-?[0-9 | .]+\))/g;
             break;
         case "pf":
-            expression = /(sqr\(\-?[0-9 | .]+\)).(1\/\(\-?[0-9 | .]+\))/g;
+            expression = /(sqr\(\-?[0-9 | .]+\))([\+ | \x |\÷ |\-])(1\/\(\-?[0-9 | .]+\))/g;
             break;
         case "fs":
-            expression = /(1\/\(\-?[0-9 | .]+\)).(√\([0-9 | .]+\))/g;
+            expression = /(1\/\(\-?[0-9 | .]+\))([\+ | \x |\÷ |\-])(√\([0-9 | .]+\))/g;
             break;
         case "sf":
-            expression = /(√\([0-9 | .]+\)).(1\/\(\-?[0-9 | .]+\))/g;
+            expression = /(√\([0-9 | .]+\))([\+ | \x |\÷ |\-])(1\/\(\-?[0-9 | .]+\))/g;
             break;
         case "sp":
-            expression = /(√\([0-9 | .]+\)).(sqr\(\-?[0-9 | .]+\))/g;
+            expression = /(√\([0-9 | .]+\))([\+ | \x |\÷ |\-])(sqr\(\-?[0-9 | .]+\))/g;
             break;
         case "ps":
-            expression = /(sqr\(\-?[0-9 | .]+\)).(√\([0-9 | .]+\))/g;
+            expression = /(sqr\(\-?[0-9 | .]+\))([\+ | \x |\÷ |\-])(√\([0-9 | .]+\))/g;
             break;
         case "numSign":
             expression = /([0-9 | .]+[\+ | \x | \÷ |\-])/g
