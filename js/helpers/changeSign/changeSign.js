a3cd0966037ade873d57267df7fd8e286bde15f0
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-12-08 10:12:42 -0300

    changed the pos of vector after tweaks a bit regular expressions

--- js/helpers/changeSign/changeSign.js
+++ js/helpers/changeSign/changeSign.js
@@ -4,12 +4,11 @@ import testExpression from "../testExpression/testExpression.js";
 function changeSign(sign,stateCopy){
     
     let result = new String(stateCopy.result);
-    let ExecEndsWithSign = execExpression(stateCopy,"endsWithSign");
     let ExecSignBetween = execExpression(stateCopy,"signBetween");
 
     (testExpression(stateCopy,"testEqual") === true) ? stateCopy.operation = result + sign : 
-    ((testExpression(stateCopy,"testSpecial") === true && sign!==ExecSignBetween[2])) ? stateCopy.operation = ExecSignBetween[1] + sign + ExecSignBetween[3] 
-    : stateCopy.operation = ExecEndsWithSign[1] + sign;
+    ((testExpression(stateCopy,"testSpecial") === true && sign!==ExecSignBetween[3])) ? stateCopy.operation = ExecSignBetween[1] + sign + ExecSignBetween[4] 
+    : stateCopy.operation = execExpression(stateCopy,"endsWithSign")[1] + sign;
 }
 
 export default changeSign;
