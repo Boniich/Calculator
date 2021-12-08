1a11c725eeab1a9c8b4fcadf9306cb90213f4cc1
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-12-08 10:58:34 -0300

    mathSigns was re-worked, now it use just one regular expression for some if

--- js/functionalities/mathSigns/mathSigns.js
+++ js/functionalities/mathSigns/mathSigns.js
@@ -1,3 +1,5 @@
+import changeSign from "../../helpers/changeSign/changeSign.js";
+import execExpression from "../../helpers/execExpression/execExpression.js";
 import testExpression from "../../helpers/testExpression/testExpression.js";
 import makingAnOperation from "../makingAnOperation/makingAnOperation.js";
 import deletePoint from "../point/deletePoint/deletePoint.js";
@@ -5,15 +7,10 @@ import deletePoint from "../point/deletePoint/deletePoint.js";
 const mathSigns = (flag,stateCopy,sign) =>{
 
     let equal = false;
-    let operation = new String(stateCopy.operation);
     let result = new String(stateCopy.result);
-    let endParentesis = operation.endsWith(")");
+    let endParentesis = testExpression(stateCopy,"endParentesis");
     let point = result.endsWith(".");
     let pointAndZero = result.endsWith(".0");
-
-    let determineSign = /(1?[\/ | \√ | sqr]*\(?\-?[0-9 | 0.0]+\)?)([\+ | \x |\÷ |\% |\= |\-])/g;
-    let thisSignIs = determineSign.exec(operation);
-    console.log("array",thisSignIs);
 
 
     if(point === true || pointAndZero === true){
@@ -29,26 +26,22 @@ const mathSigns = (flag,stateCopy,sign) =>{
 
         stateCopy.operation = "";
         stateCopy.result = "0";
-    }else if(endParentesis === true && testExpression(stateCopy,"2f") === false && testExpression(stateCopy,"nf") === false
-             && testExpression(stateCopy,"pp") === false && testExpression(stateCopy,"np") === false && testExpression(stateCopy,"ss") === false 
-             && testExpression(stateCopy,"ns") === false && testExpression(stateCopy,"fp") === false && testExpression(stateCopy,"pf") === false && 
-             testExpression(stateCopy,"fs") === false && testExpression(stateCopy,"sf") === false && testExpression(stateCopy,"sp") === false && testExpression(stateCopy,"ps") === false){
+    }else if(endParentesis === true && testExpression(stateCopy,"fractionPotencySquare") === false){
 
         console.log("add sign if end with ')'")
         stateCopy.operation += sign;
 
-    }else if(sign !== thisSignIs[2]){
+    }else if(sign !== execExpression(stateCopy,"endsWithSign")[2]
+     || (testExpression(stateCopy,"testSpecial") && sign!==execExpression(stateCopy,"signBetween")[3]) 
+     || testExpression(stateCopy,"testEqual")){
 
-        console.log("Aca vamos a cambiar el signos");
-        stateCopy.operation = thisSignIs[1] + sign;
+        console.log("here will change sign");
+        changeSign(sign,stateCopy);
 
     }else{
 
         makingAnOperation(flag,stateCopy,equal);
     }
-
- 
-
 
 
 }
