84c575bdce7ba06d3e1c1ee9fde3192726e6c444
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-12-12 14:13:49 -0300

    the name of those folders and function was changed

--- js/helpers/history/deteleHistory/deleteHistory.js
+++
@@ -1,9 +0,0 @@
-import getElement from "../../getElement/getElement.js";
-
-function deteleHistory(){
-    getElement("list").innerHTML = "";
-    getElement("no-history").style.display = "block";
-    getElement("hide-delete-btn").style.display = "none";
-}
-
-export default deteleHistory;
